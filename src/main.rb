97030a5a7335c4cbe47015ea522ac85013d300cf
Joshua-Maurici <joshua.maurici93@gmail.com>
2021-12-16 15:33:50 +1100

    added 3 second pause after starting quiz message

--- src/main.rb
+++ src/main.rb
@@ -8,22 +8,30 @@ require_relative './methods.rb'
 
 
 puts "Please tell me your name?"
-user_name = gets.chomp.capatilize.colorize(:blue)
+user_name = gets.chomp.capitalize.colorize(:cyan)
 
 system("clear")
-my_favourite_pokemon()
+
 
 while true
     puts "Welcome #{user_name} to the Pokemon Starter Quiz!. What would you like to do?"
+    my_favourite_pokemon(user_name)
     puts "Press 1| to start the quiz, Press 2| to view the README, Press 3| to QUIT"
     user_input = gets.chomp.to_i    
 case user_input
     when 1
         puts "Starting the Quiz!!!!".colorize(:green)
+        sleep(1)
+        puts ".".colorize(:green)
+        sleep(1)
+        puts ".".colorize(:green)
+        sleep(1)
+        puts ".".colorize(:green)
+        sleep(1)
+        system("clear")
         quiz()
         puts "Thank you for playing."
         exit
-
     when 2
         puts File.read("../README.md")
         
--- src/methods.rb
+++ src/methods.rb
@@ -1,13 +1,20 @@
-def secret_pokemon
-    legendry_pokemon = ["Articuno", "Moltres", "Zapdos"]
-    if user_name == legendry_pokemon
-        puts "You have found one of the legendry pokemon "#{legendry_pokemon"
+# def secret_pokemon
+#     legendry_pokemon = ["Articuno", "Moltres", "Zapdos"]
+#     if user_name == legendry_pokemon
+#         puts "You have found one of the legendry pokemon "#{legendry_pokemon"
 
+#     end
+
+# end
+
+def my_favourite_pokemon(name)
+    if name == "Arcanine"
+        puts "You have found the developers favourite pokemon ARCANINE!".colorize(:yellow)
+    elsif name == "Zapdos"
+        puts "KAAAWWW!!!!! you've found one of the legendary birds!".colorize(:light_yellow)
+    elsif name == "Articuno"
+        puts "KAAAWWW!!!!! you've found one of the legendary birds!".colorize(:cyan)
+    elsif name == "Moltress"
+        puts "KAAAWWW!!!!! you've found one of the legendary birds!".colorize(:red)
     end
-
-end
-
-def my_favourite_pokemon
-if user_name == "Arcanine"
-    puts "You have found the developers favourite pokemon ARCANINE!".colorize(:yellow)
 end
